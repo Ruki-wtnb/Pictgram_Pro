3a4d55922ac56fc2691a51e3819ef9e70ded3cef
Ruki-wtnb <shinpeww@gmail.com>
2019-10-30 15:21:13 +0000

    passwordのバリデーションが残

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   end
   
   def create
-
+    
     @user = User.new(user_params)
     
     if @user.save
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   
   private
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   
-  validates :name, presence: true
-  validates :email, presence: true
+  validates :name, presence: true, length:{maximum:15}
+  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  
+  validates :password, length:{in: 8..32}
+  
+  has_secure_password
 end
