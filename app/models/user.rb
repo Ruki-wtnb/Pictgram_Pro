3ffa0c48acd4a204e0c5ed6f150d917e165d6d8c
Ruki-wtnb <shinpeww@gmail.com>
2019-11-01 12:22:55 +0000

    文字数修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
   
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,100}+\z/i
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{8,32}+\z/i
   validates :password, length:{in: 8..32}, format: { with: VALID_PASSWORD_REGEX }
   
   has_secure_password
